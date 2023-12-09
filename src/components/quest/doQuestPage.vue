72ac835c4a706fe373d7276130473419241ddfc5
zz0257800000 <zz0257800000@gmail.com>
2023-12-09 12:48:51 +0800

    1248

--- src/components/quest/doQuestPage.vue
+++ src/components/quest/doQuestPage.vue
@@ -14,31 +14,34 @@ export default {
       questArr: [], // 你的初始化数据
       doquestArr: [], //現在更改為陣列
       checkinfo: {}, // 确保初始化为空对象
+      ABC: [], // 你的初始化数据
+      selectedRadio: null, // 用于存储单选题目的选择
+
 
     };
   },
   created() {
     if (this.searchAllList.questionList) {
-    this.checkinfo = {};
-    for (const question of this.searchAllList.questionList) {
-      if (question.questionType === 'text') {
-        this.$set(this.checkinfo, question.questionId, '');
-      } else if (question.optionsType === 'radio' || question.optionsType === 'checkbox') {
-        for (let i = 0; i < question.options.split(';').length; i++) {
-          const key = question.questionId + '_' + i;
-          this.$set(this.checkinfo, key, ''); // 初始化为空字符串
+      this.checkinfo = {};
+      for (const question of this.searchAllList.questionList) {
+        if (question.questionType === 'text') {
+          this.$set(this.checkinfo, question.questionId, '');
+        } else if (question.optionsType === 'radio' || question.optionsType === 'checkbox') {
+          for (let i = 0; i < question.options.split(';').length; i++) {
+            const key = question.questionId + '_' + i;
+            this.$set(this.checkinfo, key, ''); // 初始化为空字符串
+          }
         }
       }
     }
-  }
   },
   mounted() {
-    
+
     this.getQuizInfo();
 
   },
   methods: {
-  getQuizInfo() {
+    getQuizInfo() {
       const questionnaireIdToFind = this.$route.params.wantId;
       this.questionnaireId = questionnaireIdToFind;
 
@@ -85,7 +88,7 @@ export default {
         .catch((error) => console.error('Error:', error));
     },
     // 在 processSelectedOptions 方法中
- processSelectedOptions() {
+    processSelectedOptions() {
       const selectedOptions = [];
 
       for (const key in this.checkinfo) {
@@ -142,7 +145,7 @@ export default {
       }
 
       console.log(`Answer: ${ans}`);
-      this.hi = ans;
+      this. str = ans;
     },
     // 新增一个方法用于检查是否为文本类型问题
     isTextQuestion(questionId) {
@@ -154,14 +157,17 @@ export default {
       try {
 
         if (!this.doquestArr.name || !this.doquestArr.phoneNumder || !this.doquestArr.email) {
-      // 提示用户输入姓名、电话号码和电子邮件
-      // 你可以使用适当的 UI 库或弹出框库来显示提示信息
-      alert("需要全部輸入");
+          // 提示用户输入姓名、电话号码和电子邮件
+          // 你可以使用适当的 UI 库或弹出框库来显示提示信息
+          alert("需要全部輸入");
 
-      console.error('Name, PhoneNumber, and Email are required');
-      return;
-    }      // 提示用户输入姓名、电话号码和电子邮件
+          return;
+        }      // 提示用户输入姓名、电话号码和电子邮件
         this.processSelectedOptions();
+        let str = "";
+        this.ABC.forEach(e => {
+          str = str + e + "";
+        })
         const userResponse = {
           userList: [
             {
@@ -169,10 +175,11 @@ export default {
               phoneNumber: this.doquestArr.phoneNumder,
               name: this.doquestArr.name,
               email: this.doquestArr.email,
-              ans: this.hi
+              ans: str
             }
           ]
         };
+
         console.log('userResponse:', userResponse); // 添加这行
         const response = await fetch('http://localhost:8080/api/quiz/usercreate', {
           method: 'POST',
@@ -204,6 +211,40 @@ export default {
       }
     },
 
+    Checkbox(ONE) {
+  // 获取前一个值
+  const lastValue = this.ABC[this.ABC.length - 1];
+
+  // 判断是否需要添加 '!'
+  const newValue = lastValue ? `!${ONE}` : ONE;
+
+  // 添加新的值
+  this.ABC.push(newValue + '');
+},
+
+Radio(ZER0) {
+
+  // 如果数组不为空，在前一个值后面添加 ';'
+  if (this.ABC.length > 0) {
+    this.ABC[this.ABC.length - 1] += ';';
+  }
+
+ // 如果 ZER0 未定义或为空字符串，就返回 'null'
+  const radioValue = ZER0 !== undefined && ZER0 !== '' ? ZER0 : 'null';
+
+  // 清空 ZER0 的值
+  ZER0 = '';
+
+  // 添加新数据到数组
+  this.ABC.push(radioValue);
+},
+Text(questionId) {
+  const textValue = this.checkinfo[questionId];
+  // 將文本框的值添加到 this.ABC 數組中，如果 textValue 為空，就返回 'null'
+  this.ABC.push(textValue !== '' ? textValue : 'null' + ';');
+},
+
+
   },
 };
 </script>
@@ -213,7 +254,8 @@ export default {
     <div class="doQuestHeader" v-if="searchAllList.questionnaire">
       <span>{{ searchAllList.questionnaire.startDate }}~~{{ searchAllList.questionnaire.endDate }}</span>
       <h6>問卷名稱:{{ searchAllList.questionnaire.title }}</h6>
-      <h6>問卷描述:{{ searchAllList.questionnaire.description }}</h6>
+      <h6>問卷描述:{{ searchAllList.questionnaire.description }}</h6>   {{ JSON.stringify(this.ABC) }}
+
     </div>
     <div class="backToFix" v-if="page === 1">
       <div class="fixedQuest">
@@ -240,7 +282,7 @@ export default {
           <div v-if="question.optionsType === 'radio'">
             <div v-for="(option, optionIndex) in question.options.split(';')" :key="optionIndex">
               <input type="radio" :id="'q_' + questionIndex + '_o_' + optionIndex" :value="option"
-                v-model="checkinfo[question.questionId + '_radio_' + optionIndex]" />
+                v-model="checkinfo[question.questionId + '_radio_' + optionIndex]" name="A" @click="Radio(option)" />
               <label :for="'q_' + questionIndex + '_o_' + optionIndex">{{ option }}</label>
             </div>
           </div>
@@ -249,13 +291,13 @@ export default {
           <div v-else-if="question.optionsType === 'checkbox'">
             <div v-for="(option, optionIndex) in question.options.split(';')" :key="optionIndex">
               <input type="checkbox" :id="'q_' + questionIndex + '_o_' + optionIndex" :value="option"
-                v-model="checkinfo[question.questionId + '_o_' + optionIndex]" />
+                v-model="checkinfo[question.questionId + '_o_' + optionIndex]" @click="Checkbox(option)" />
               <label :for="'q_' + questionIndex + '_o_' + optionIndex">{{ option }}</label>
             </div>
           </div>
 
-          <div v-else-if="question.optionsType === 'text'"  class="input-wrapper">
-            <input v-model="checkinfo[question.questionId]" type="text" />
+          <div v-else-if="question.optionsType === 'text'" class="input-wrapper">
+            <input v-model="checkinfo[question.questionId]" type="text" @blur="Text(question.questionId)" />
           </div>
         </div>
 
@@ -268,8 +310,10 @@ export default {
 
 <style lang="scss" scoped>
 .submit-button {
-  bottom: 20px; /* 距离底部 20px */
-  right: 20px; /* 距离右侧 20px */
+  bottom: 20px;
+  /* 距离底部 20px */
+  right: 20px;
+  /* 距离右侧 20px */
   background-color: #06404e;
   color: white;
   padding: 10px 20px;
@@ -286,6 +330,7 @@ export default {
     background-color: rgb(69, 160, 160);
   }
 }
+
 .doQuestPageBody {
   display: flex;
   flex-direction: column;
@@ -364,14 +409,18 @@ export default {
 .fluidQuest button:hover {
   background-color: #2980b9;
 }
+
 .input-wrapper input[type="text"] {
-  width: 550px; /* 你想要的宽度 */
-height: 100px;
-  padding: 10px; /* 可选的内边距 */
+  width: 550px;
+  /* 你想要的宽度 */
+  height: 100px;
+  padding: 10px;
+  /* 可选的内边距 */
   /* 其他样式... */
   white-space: pre-wrap;
-  resize: vertical; /* 或者使用 horizontal，根据需要调整 */
-  max-height: 200px; /* 设置输入框的最大高度，根据需要调整 */
+  resize: vertical;
+  /* 或者使用 horizontal，根据需要调整 */
+  max-height: 200px;
+  /* 设置输入框的最大高度，根据需要调整 */
 
-}
-</style>
+}</style>
